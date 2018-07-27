dc946278a5ef5f12eb55dd164323a9af22098359
conhy <conhysham@cox.net>
2018-07-27 10:01:26 -0700

    Changing connection

--- config/connection.js
+++ config/connection.js
@@ -2,8 +2,8 @@
 var Sequelize = require("sequelize");
 
 // Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
-var sequelize = new Sequelize("burgers_db", "root", "password", {
-  host: "localhost",
+var sequelize = new Sequelize("r91q40wnkaf3hkqx", "dowkmswz7l0fhkqm", "k9xsp2rvrf1aaxbb", {
+  host: "ofcmikjy9x4lroa2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
   dialect: "mysql",
   pool: {
